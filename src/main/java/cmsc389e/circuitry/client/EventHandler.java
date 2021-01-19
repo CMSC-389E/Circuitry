d60212bd37f199df908c56da0480dff1b2dc109b
Jamie Brassel <jbras3@gmail.com>
2021-01-18 19:26:44 -0500

    Optimize EventHandlers and temporarily remove default world settings

--- src/main/java/cmsc389e/circuitry/client/EventHandler.java
+++ src/main/java/cmsc389e/circuitry/client/EventHandler.java
@@ -5,7 +5,6 @@ import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import com.google.common.collect.ImmutableSet;
-import com.mojang.datafixers.types.JsonOps;
 
 import cmsc389e.circuitry.Circuitry;
 import cmsc389e.circuitry.common.network.KeyMessage;
@@ -13,9 +12,6 @@ import cmsc389e.circuitry.common.network.KeyMessage.Key;
 import cmsc389e.circuitry.common.network.PacketHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.AlertScreen;
-import net.minecraft.client.gui.screen.CreateWorldScreen;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.util.Util;
 import net.minecraft.util.Util.OS;
 import net.minecraft.util.math.BlockPos;
@@ -25,11 +21,6 @@ import net.minecraft.util.math.RayTraceResult.Type;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraft.world.GameType;
-import net.minecraft.world.WorldSettings;
-import net.minecraft.world.WorldType;
-import net.minecraft.world.gen.FlatGenerationSettings;
-import net.minecraft.world.storage.WorldInfo;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.event.TickEvent;
@@ -57,35 +48,24 @@ public class EventHandler {
 
 	@SubscribeEvent
 	public static void onGuiOpenEvent(GuiOpenEvent event) {
-		Screen screen = event.getGui();
-		if (screen instanceof CreateWorldScreen)
-			((CreateWorldScreen) screen).recreateFromExistingWorld(
-					new WorldInfo(new WorldSettings(0, GameType.CREATIVE, false, false, WorldType.FLAT).enableCommands()
-							.setGeneratorOptions(FlatGenerationSettings.createFlatGeneratorFromString(
-									"minecraft:bedrock,3*minecraft:stone,52*minecraft:sandstone;minecraft:desert")
-									.func_210834_a(JsonOps.INSTANCE).getValue()),
-							I18n.format("selectWorld.newWorld")));
-		else {
-			ModList list = ModList.get();
+		ModList list = ModList.get();
 
-			Set<String> allowed = ImmutableSet.of(Circuitry.MODID, ForgeVersion.MOD_ID, "minecraft");
-			String mods = list.applyForEachModContainer(ModContainer::getModInfo).parallel()
-					.filter(info -> !allowed.contains(info.getModId())).map(IModInfo::getDisplayName)
-					.collect(Collectors.joining(", "));
-			if (!mods.isEmpty())
-				event.setGui(alert("Illegal mods are installed.",
-						"You must delete the following mods before proceeding:", mods, "fml.button.open.mods.folder",
-						os -> os.openFile(FMLPaths.MODSDIR.get().toFile())));
+		Set<String> allowed = ImmutableSet.of(Circuitry.MODID, ForgeVersion.MOD_ID, "minecraft");
+		String mods = list.applyForEachModContainer(ModContainer::getModInfo).parallel()
+				.filter(info -> !allowed.contains(info.getModId())).map(IModInfo::getDisplayName)
+				.collect(Collectors.joining(", "));
+		if (!mods.isEmpty())
+			event.setGui(alert("Illegal mods are installed.", "You must delete the following mods before proceeding:",
+					mods, "fml.button.open.mods.folder", os -> os.openFile(FMLPaths.MODSDIR.get().toFile())));
 
-			IModInfo mod = list.getModContainerById(Circuitry.MODID).get().getModInfo();
-			if (!mod.getVersion().getQualifier().equals("NONE")) {
-				CheckResult result = VersionChecker.getResult(mod);
-				if (result.status == Status.OUTDATED)
-					event.setGui(alert(mod.getDisplayName() + " is out of date.",
-							"You must update to version " + result.target
-									+ " before proceeding.\nOpen Link will bring you to the below page:",
-							result.url, "Open Link", os -> os.openURI(result.url)));
-			}
+		IModInfo mod = list.getModContainerById(Circuitry.MODID).get().getModInfo();
+		if (!mod.getVersion().getQualifier().equals("NONE")) {
+			CheckResult result = VersionChecker.getResult(mod);
+			if (result.status == Status.OUTDATED)
+				event.setGui(alert(mod.getDisplayName() + " is out of date.",
+						"You must update to version " + result.target
+								+ " before proceeding.\nOpen Link will bring you to the below page:",
+						result.url, "Open Link", os -> os.openURI(result.url)));
 		}
 	}
 
@@ -93,14 +73,17 @@ public class EventHandler {
 	@SuppressWarnings("resource")
 	public static void onTickClient(TickEvent.ClientTickEvent event) {
 		if (event.phase == Phase.END) {
-			Minecraft minecraft = Minecraft.getInstance();
-			RayTraceResult result = minecraft.objectMouseOver;
-			if (result != null && result.getType() == Type.BLOCK) {
-				BlockPos pos = ((BlockRayTraceResult) result).getPos();
-				for (Key key : Key.values())
-					if (key.binding.isPressed())
-						PacketHandler.CHANNEL.sendToServer(new KeyMessage(key, pos));
-			}
+			BlockPos pos = null;
+			for (Key key : Key.values())
+				while (key.binding.isPressed()) {
+					if (pos == null) {
+						Minecraft minecraft = Minecraft.getInstance();
+						RayTraceResult result = minecraft.objectMouseOver;
+						if (result != null && result.getType() == Type.BLOCK)
+							pos = ((BlockRayTraceResult) result).getPos();
+					}
+					PacketHandler.CHANNEL.sendToServer(new KeyMessage(key, pos));
+				}
 		}
 	}
 }
--- src/main/java/cmsc389e/circuitry/common/EventHandler.java
+++ src/main/java/cmsc389e/circuitry/common/EventHandler.java
@@ -13,14 +13,10 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.world.Difficulty;
-import net.minecraft.world.GameRules;
 import net.minecraft.world.dimension.DimensionType;
-import net.minecraft.world.storage.WorldInfo;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.TickEvent.Phase;
 import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
-import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
@@ -51,7 +47,7 @@ public class EventHandler {
 
 	@SubscribeEvent
 	public static void onTickPlayer(TickEvent.PlayerTickEvent event) {
-		if (event.phase == Phase.START && event.side.isServer()) {
+		if (event.phase == Phase.END && event.side.isServer()) {
 			TileEntity entity = event.player.world.getTileEntity(((BlockRayTraceResult) event.player
 					.pick(event.player.getAttribute(PlayerEntity.REACH_DISTANCE).getValue(), 1, false)).getPos());
 			event.player.sendStatusMessage(
@@ -60,18 +56,5 @@ public class EventHandler {
 							: ""),
 					true);
 		}
-	}
-
-	@SubscribeEvent
-	public static void onWorldCreateSpawnPosition(WorldEvent.CreateSpawnPosition event) {
-		WorldInfo info = event.getWorld().getWorldInfo();
-		info.setDayTime(6000);
-		info.setDifficulty(Difficulty.PEACEFUL);
-
-		GameRules rules = info.getGameRulesInstance();
-		rules.get(GameRules.DO_DAYLIGHT_CYCLE).set(false, null);
-		rules.get(GameRules.DO_MOB_SPAWNING).set(false, null);
-		rules.get(GameRules.DO_TILE_DROPS).set(false, null);
-		rules.get(GameRules.DO_WEATHER_CYCLE).set(false, null);
 	}
 }
